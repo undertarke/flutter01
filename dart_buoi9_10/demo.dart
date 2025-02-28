f0ec16f7e01aba5a29dff4f8bcc50738cf5c7833
undertarke <sangrom2003@gmail.com>
2025-02-28 21:15:56 +0700

    buoi 11

--- dart_buoi9_10/demo.dart
+++ dart_buoi9_10/demo.dart
@@ -1,8 +1,31 @@
+import 'dart:convert';
+
 void main(List<String> args) {
-  print('Never odd or even'.lastIndexOf('d'));
+  // print('Never odd or even'.lastIndexOf('d'));
   //         tony
   // print(("        tony          "));
   // print("".isNotEmpty);
-  var a = "";
-  if (a.isNotEmpty) {}
+  // var a = "";
+  // if (a.isNotEmpty) {}
+
+  // String => json
+  var json = jsonDecode('{ "id":123, "hoTen":{"id":999,"hoTen":true} }');
+
+  var object = {"id": 101, "name": "john"};
+  jsonEncode(object);
+
+  print(json["hoTen"]["id"]);
+
+  A a = A();
+}
+
+class A {
+  var _a;
+  var _b;
+  // getter
+  //setter
+  dynamic func() {
+    this._a;
+    return 1;
+  }
 }
