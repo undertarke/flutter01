fbef2929ce353e4fceb825da167ad60c22b23594
undertarke <sangrom2003@gmail.com>
2025-03-15 23:45:23 +0700

    buoi 14

--- flutter_begin/lib/main.dart
+++ flutter_begin/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_buoi11/baitap_text.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        // backgroundColor: Colors.blue,
+        backgroundColor: Colors.orange,
         body: SafeArea(
-          child: RichTextWidget(),
+          child: ImageWidget(),
         ),
       ),
       debugShowCheckedModeBanner: false,
@@ -64,27 +65,30 @@ class RichTextWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RichText(
-      text: const TextSpan(
-        text: "Tôi tên là: ",
-        style: TextStyle(color: Colors.black, fontSize: 20),
-        children: [
-          TextSpan(
-            text: "Nguyễn Văn A",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
+    return Container(
+      color: Colors.red,
+      child: RichText(
+        text: const TextSpan(
+          text: "Tôi tên là: ",
+          style: TextStyle(color: Colors.black, fontSize: 20),
+          children: [
+            TextSpan(
+              text: "Nguyễn Văn A",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          TextSpan(text: ", fb: "),
-          TextSpan(
-            text: "facebook.com/nguyenvana",
-            style: TextStyle(
-              color: Colors.blue,
-              decoration: TextDecoration.underline,
-              fontStyle: FontStyle.italic,
-            ),
-          )
-        ],
+            TextSpan(text: ", fb: "),
+            TextSpan(
+              text: "facebook.com/nguyenvana",
+              style: TextStyle(
+                color: Colors.blue,
+                decoration: TextDecoration.underline,
+                fontStyle: FontStyle.italic,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -102,10 +106,121 @@ class RichTextWidget extends StatelessWidget {
 //   }
 // }
 
-
 // code snippet => stl
 
 // alt + Shift + F => beautiful format code
 // bóng đè: giúp thêm và xóa nhanh widget lồng nhau
 
+// buổi 14:
 
+class IconWidget extends StatelessWidget {
+  const IconWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      FontAwesomeIcons.ccApplePay,
+    );
+  }
+}
+
+class ImageWidget extends StatelessWidget {
+  const ImageWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // return Image.asset("./assets/imgs/cat.jpg");
+    return Container(
+      color: Colors.blue,
+      child: Image.network(
+        "https://airbnbnew.cybersoft.edu.vn/images/phong2.png",
+        width: 400,
+        height: 150,
+        fit: BoxFit.fill,
+        errorBuilder: (context, error, stackTrace) {
+          return Image.asset("./assets/imgs/404.jpg");
+        },
+      ),
+    );
+  }
+}
+
+class CardWidget extends StatelessWidget {
+  const CardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(50),
+      color: Colors.red,
+      shadowColor: Colors.blue,
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(50),
+        child: Text("Demo card !!!"),
+      ),
+    );
+  }
+}
+
+class ContainerWidget extends StatelessWidget {
+  const ContainerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200, // pixel
+      height: 100,
+      // alignment: Alignment.bottomRight,
+      child: Text(
+        "Căn chỉnh nội dung Căn chỉnh nội dung con bên trong Container (ví dụ: Alignment.center,a sdada dasd dsa das das dasd Alignment.topLeft). Căn chỉnh nội dung con bên trong Container (ví dụ: Alignment.center, Alignment.topLeft).",
+        style: TextStyle(color: Colors.black),
+      ),
+      decoration: BoxDecoration(
+        color: Colors.red,
+        // borderRadius: BorderRadius.circular(100),
+        border: Border.all(color: Colors.yellow, width: 10),
+        gradient: const LinearGradient(
+          colors: [
+            Color.fromARGB(255, 2, 66, 176),
+            Colors.green,
+          ],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+      ),
+      // foregroundDecoration: BoxDecoration(
+      //   border: Border.all(color: Colors.yellow, width: 10),
+      //   color: Colors.orange,
+      // ),
+      clipBehavior: Clip.antiAlias,
+    );
+  }
+}
+
+class MarginPaddingWidget extends StatelessWidget {
+  const MarginPaddingWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(50),
+      child: Container(
+        margin: EdgeInsets.all(50),
+        padding: EdgeInsets.symmetric(horizontal: 50),
+        width: 200,
+        height: 200,
+        color: Colors.green,
+        child: Text(
+          "Content",
+          style: TextStyle(
+            backgroundColor: Colors.red,
+          ),
+        ),
+      ),
+    );
+  }
+}
