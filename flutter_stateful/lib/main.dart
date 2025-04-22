91e9e7b24ea4f84341b68ddf6c8a156ec3f81b56
undertarke <33415772+undertarke@users.noreply.github.com>
2025-04-22 14:31:33 +0700

    buoi 23

--- flutter_stateful/lib/main.dart
+++ flutter_stateful/lib/main.dart
@@ -11,7 +11,176 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: SafeArea(child: MyWidget())),
+      home: Scaffold(body: SafeArea(child: BaiTapButtonWidget())),
+    );
+  }
+}
+
+class BaiTapButtonWidget extends StatefulWidget {
+  const BaiTapButtonWidget({super.key});
+
+  @override
+  State<BaiTapButtonWidget> createState() => _BaiTapButtonWidgetState();
+}
+
+class _BaiTapButtonWidgetState extends State<BaiTapButtonWidget> {
+  var _color;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _color = Colors.green;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(height: 200, width: 200, color: _color),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _color = Colors.green;
+                });
+              },
+              child: Text(""),
+              style: TextButton.styleFrom(
+                // padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                backgroundColor: Colors.green,
+                fixedSize: Size(100, 100),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                   _color = Colors.red;
+                });
+              },
+              child: Text(""),
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.all(50),
+                backgroundColor: Colors.red,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                   _color = Colors.yellow;
+                });
+              },
+              child: Text(""),
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.all(50),
+                backgroundColor: Colors.yellow,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+// buoi 23
+
+class ElevatedButtonWidget extends StatelessWidget {
+  const ElevatedButtonWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.yellow,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+          onPressed: () {
+            print("button clicked");
+          },
+          child: Text("Elevated button"),
+        ),
+
+        TextButton(
+          style: TextButton.styleFrom(),
+          onPressed: () {
+            print("text button clicked");
+          },
+          child: Text("Text button"),
+        ),
+
+        OutlinedButton(
+          style: TextButton.styleFrom(),
+          onPressed: () {
+            print("outline button clicked");
+          },
+          child: Text("Outline button"),
+        ),
+      ],
+    );
+  }
+}
+
+class _BaiTapTangGiamSoState extends StatefulWidget {
+  const _BaiTapTangGiamSoState({super.key});
+
+  @override
+  State<_BaiTapTangGiamSoState> createState() => __BaiTapTangGiamSoStateState();
+}
+
+class __BaiTapTangGiamSoStateState extends State<_BaiTapTangGiamSoState> {
+  var number;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    number = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("$number", style: TextStyle(fontSize: 50)),
+        ListTile(
+          title: Text("+", style: TextStyle(fontSize: 50, color: Colors.green)),
+          onTap: () {
+            setState(() {
+              // number = number + 1;
+              number += 1;
+            });
+          },
+        ),
+        ListTile(
+          title: Text("-", style: TextStyle(fontSize: 50, color: Colors.red)),
+          onTap: () {
+            setState(() {
+              // number = number - 1;
+              number -= 1;
+            });
+          },
+        ),
+      ],
     );
   }
 }
@@ -24,7 +193,15 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
-  var title = "Chưa thay đổi";
+  var title;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    title = "Chưa thay đổi";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,16 +211,34 @@ class _MyWidgetState extends State<MyWidget> {
         ListTile(
           title: Text("Click me"),
           onTap: () {
-            title = "Đã thay đổi";
-            print(title);
+            // title = "Đã thay đổi";
+            // print(title);
             // giúp rebuild (load lại phương thức build() ) , gán lại giá trị
-            // setState(() {
-            //   title="Đã thay đổi"; // thay đổi giá trị
-            // });
+            setState(() {
+              title = "Đã thay đổi"; // thay đổi giá trị
+            });
           },
         ),
       ],
     );
+  }
+
+  @override
+  void didUpdateWidget(covariant MyWidget oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
   }
 }
 
